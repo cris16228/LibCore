87d1a0764e370405bc552ef8584289383d39fa46
cris16228 <cpiva16@gmail.com>
2024-08-16 23:54:02 +0200

    0.0.8

---
+++ library/src/main/java/com/github/cris16228/libcore/models/PieChartModel.java
@@ -0,0 +1,32 @@
+package com.github.cris16228.libcore.models;
+
+public class PieChartModel {
+
+    private String title;
+    private double value;
+    private int color;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTiley(String title) {
+        this.title = title;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+}
---
+++ library/src/main/java/com/github/cris16228/libcore/view/PieChart.java
@@ -0,0 +1,198 @@
+package com.github.cris16228.libcore.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Scroller;
+
+import com.github.cris16228.libcore.FloatUtils;
+import com.github.cris16228.libcore.R;
+import com.github.cris16228.libcore.models.PieChartModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PieChart extends View {
+
+    double totalAmount = 0;
+    private List<PieChartModel> pieChartModels = new ArrayList<>();
+    private List<String> titles = new ArrayList<>();
+    private int dataSize;
+    private Scroller scroller;
+    private int legendScrollY = 0;
+    private float startY = 0f;
+    private int pieChartSize;
+    private int pieChartXOffset;
+    private int pieChartYOffset;
+    private int legendWidth;
+    private boolean enablePercent;
+    private boolean enableLegend;
+    private float lastTouchY;
+
+
+    public PieChart(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        scroller = new Scroller(context);
+        TypedArray attributes = context.obtainStyledAttributes(attrs, R.styleable.PieChart);
+        pieChartSize = attributes.getDimensionPixelSize(R.styleable.PieChart_pieChartSize, 200);
+        pieChartSize = Math.min(pieChartSize, 475);
+        pieChartXOffset = attributes.getDimensionPixelSize(R.styleable.PieChart_pieChartXOffset, 0);
+        pieChartYOffset = attributes.getDimensionPixelSize(R.styleable.PieChart_pieChartYOffset, 0);
+        enablePercent = attributes.getBoolean(R.styleable.PieChart_enablePercent, true);
+        enableLegend = attributes.getBoolean(R.styleable.PieChart_enableLegend, true);
+        attributes.recycle();
+    }
+
+    public void setData(List<PieChartModel> pieChartModels) {
+        this.pieChartModels.clear();
+        this.titles.clear();
+        this.pieChartModels = pieChartModels;
+        dataSize = pieChartModels.size();
+    }
+
+    public void setEnablePercent(boolean enablePercent) {
+        this.enablePercent = enablePercent;
+    }
+
+    public void setEnableLegend(boolean enableLegend) {
+        this.enableLegend = enableLegend;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        int desiredWidth = widthSize;
+
+        int legendHeight = dataSize * 60 + 50;
+        int desiredHeight = heightSize - legendHeight; // Available height for the pie chart
+
+        int width = desiredWidth;
+        int height = desiredHeight + legendHeight;
+
+        if (widthMode == MeasureSpec.EXACTLY) {
+            width = widthSize;
+        } else if (widthMode == MeasureSpec.AT_MOST) {
+            width = Math.min(desiredWidth, widthSize);
+        }
+
+        if (heightMode == MeasureSpec.EXACTLY) {
+            height = heightSize;
+        } else if (heightMode == MeasureSpec.AT_MOST) {
+            height = Math.min(desiredHeight, heightSize);
+        }
+
+        setMeasuredDimension(width, height);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if (pieChartModels == null || pieChartModels.isEmpty()) {
+            return;
+        }
+
+        for (PieChartModel model : pieChartModels) {
+            titles.add(model.getTitle());
+            /*colors.add(model.getColor());*/
+            totalAmount += model.getValue();
+        }
+
+
+        float startAngle = 0;
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setStyle(Paint.Style.FILL);
+
+        int availableWidth = getWidth();
+        pieChartSize = Math.min(pieChartSize, 475);
+        legendWidth = availableWidth - pieChartSize;
+        RectF rect = new RectF(
+                pieChartXOffset,
+                pieChartYOffset,
+                pieChartXOffset + pieChartSize,
+                pieChartYOffset + pieChartSize
+        );
+
+        // Draw pie slices
+        for (PieChartModel model : pieChartModels) {
+            paint.setColor(model.getColor());
+            float sweepAngle = (float) (model.getValue() / totalAmount * 360);
+            canvas.drawArc(rect, startAngle, sweepAngle, true, paint);
+            startAngle += sweepAngle;
+        }
+        canvas.save();
+        canvas.translate(0, legendScrollY);
+        // Draw legend
+        float legendY = rect.top + 50;
+        paint.setTextSize(40);
+        for (PieChartModel model : pieChartModels) {
+            paint.setColor(Color.parseColor(String.format("#%06X", (0xFFFFFF & model.getColor()))));
+            canvas.drawRect(getWidth() - legendWidth + 50, legendY, getWidth() - legendWidth + 100, legendY + 40, paint);
+            paint.setColor(Color.WHITE);
+            String percent = FloatUtils.getNumberFormat(((float) (model.getValue() / totalAmount) * 100), 2);
+            if (enableLegend) {
+                if (enablePercent) {
+                    canvas.drawText(getResources().getString(R.string.legend_text_percent, model.getTitle(), percent), pieChartSize + 120, legendY + 30, paint);
+                } else {
+                    canvas.drawText(getResources().getString(R.string.legend_text_no_percent, model.getTitle()), pieChartSize + 120, legendY + 30, paint);
+                }
+            }
+            legendY += 60;
+        }
+        canvas.restore();
+        if (!scroller.isFinished()) {
+            invalidate();
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (pieChartModels.size() * 60 <= getHeight()) {
+            return super.onTouchEvent(event);
+        }
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                startY = event.getY();
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                float currentY = event.getY();
+                int dy = (int) (currentY - startY);
+                scrollLegend(dy);
+                startY = currentY;
+                return true;
+        }
+
+        return super.onTouchEvent(event);
+    }
+
+    private void scrollLegend(int dy) {
+
+        int totalLegendHeight = pieChartModels.size() * 60 + 90; // Total height of the legend
+        int maxScrollY = Math.max(0, totalLegendHeight - getHeight());
+        if (dy > 0) {
+            if (legendScrollY - dy < 0) {
+                legendScrollY = 0;
+            } else {
+                legendScrollY -= dy;
+            }
+        } else {
+            if ((legendScrollY + dy) * -1 > maxScrollY) {
+                legendScrollY = maxScrollY * -1;
+            } else {
+                legendScrollY += dy;
+            }
+        }
+        invalidate();
+    }
+}
