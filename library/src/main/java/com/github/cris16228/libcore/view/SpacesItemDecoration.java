d7afc5ac769fa5c9b63ebe251d3bb8276c0bda9d
cris16228 <cpiva16@gmail.com>
2025-01-26 21:29:02 +0100

    0.2.1.5

--- library/src/main/java/com/github/cris16228/libcore/view/SpacesItemDecoration.java
+++ library/src/main/java/com/github/cris16228/libcore/view/SpacesItemDecoration.java
@@ -9,17 +9,25 @@ import androidx.recyclerview.widget.RecyclerView;
 public class SpacesItemDecoration extends RecyclerView.ItemDecoration {
 
     private final int space;
+    private final int spanCount;
 
-    public SpacesItemDecoration(int space) {
+    public SpacesItemDecoration(int space, int spanCount) {
         this.space = space;
+        this.spanCount = spanCount;
     }
 
     @Override
     public void getItemOffsets(Rect outRect, @NonNull View view,
                                @NonNull RecyclerView parent, RecyclerView.State state) {
+
+        int position = parent.getChildAdapterPosition(view);
+        int column = position % spanCount;
+
         outRect.left = space;
-        outRect.right = space;
+        outRect.right = column == spanCount - 1 ? 0 : space;
         outRect.bottom = space;
-        outRect.top = space;
+
+        if (position < spanCount)
+            outRect.top = space;
     }
 }
