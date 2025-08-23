29302027f51908f8e344c334f69ba999103026ff
cris16228 <cpiva16@gmail.com>
2025-08-23 10:32:26 +0200

    0.2.1.9

---
+++ library/src/main/java/com/github/cris16228/libcore/models/CrashModel.java
@@ -0,0 +1,68 @@
+package com.github.cris16228.libcore.models;
+
+public class CrashModel {
+
+    private String appName;
+    private long versionCode;
+    private String packageName;
+    private String versionName;
+    private String error;
+    private String stackTrace;
+    private String cause;
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public long getVersionCode() {
+        return versionCode;
+    }
+
+    public void setVersionCode(long versionCode) {
+        this.versionCode = versionCode;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
+    }
+
+    public String getVersionName() {
+        return versionName;
+    }
+
+    public void setVersionName(String versionName) {
+        this.versionName = versionName;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getStackTrace() {
+        return stackTrace;
+    }
+
+    public void setStackTrace(String stackTrace) {
+        this.stackTrace = stackTrace;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+
+    public void setCause(String cause) {
+        this.cause = cause;
+    }
+}
--- library/src/main/java/com/github/cris16228/libcore/utils/UncaughtExceptionHandler.java
+++ library/src/main/java/com/github/cris16228/libcore/utils/UncaughtExceptionHandler.java
@@ -9,9 +9,9 @@ import androidx.annotation.NonNull;
 import com.github.cris16228.libcore.AsyncUtils;
 import com.github.cris16228.libcore.FileUtils;
 import com.github.cris16228.libcore.NetworkUtils;
-import com.github.cris16228.libcore.StringUtils;
 import com.github.cris16228.libcore.deviceutils.PackageUtils;
 import com.github.cris16228.libcore.http.HttpUtils;
+import com.github.cris16228.libcore.models.CrashModel;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
@@ -24,6 +24,133 @@ import java.util.concurrent.TimeUnit;
 public class UncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
 
     private final Thread.UncaughtExceptionHandler defaultUEH;
+    private final Activity app;
+    private final String bearer;
+    private final boolean reportCrash;
+    private final String url;
+    StringBuilder report = new StringBuilder();
+
+    public UncaughtExceptionHandler(Activity app, boolean reportCrash, String url) {
+        this.defaultUEH = Thread.getDefaultUncaughtExceptionHandler();
+        this.app = app;
+        this.bearer = "";
+        this.reportCrash = reportCrash;
+        this.url = url;
+    }
+
+    public UncaughtExceptionHandler(Activity app, String bearer, boolean reportCrash, String url) {
+        this.defaultUEH = Thread.getDefaultUncaughtExceptionHandler();
+        this.app = app;
+        this.bearer = bearer;
+        this.reportCrash = reportCrash;
+        this.url = url;
+    }
+
+    @Override
+    public void uncaughtException(@NonNull Thread t, @NonNull Throwable e) {
+        CountDownLatch latch = new CountDownLatch(1);
+        AsyncUtils uploadCrash = AsyncUtils.get();
+        CrashModel crashModel = new CrashModel();
+        crashModel.setAppName(PackageUtils.with(app).getAppName(app.getPackageName()));
+        report.append(crashModel.getAppName()).append("\n");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            crashModel.setVersionCode(PackageUtils.with(app).appFromPackage(app.getPackageName()).getLongVersionCode());
+        } else {
+            crashModel.setVersionCode(PackageUtils.with(app).appFromPackage(app.getPackageName()).versionCode);
+        }
+        report.append(crashModel.getVersionCode()).append("\n");
+        crashModel.setPackageName(app.getPackageName());
+        report.append(crashModel.getPackageName()).append("\n");
+        crashModel.setVersionName(PackageUtils.with(app).appFromPackage(app.getPackageName()).versionName);
+        report.append(crashModel.getVersionName()).append("\n");
+        crashModel.setError(e.toString());
+        report.append(crashModel.getError()).append("\n");
+        StackTraceElement[] arr = e.getStackTrace();
+        StringBuilder stackTrace = getBuilder(arr);
+        report.append("-----------------------------------------------------------------------------").append("\n").append("\n");
+        crashModel.setStackTrace(stackTrace.toString());
+        Throwable cause = e.getCause();
+        arr = cause.getStackTrace();
+        StringBuilder causeReport = getStringBuilder(arr);
+        crashModel.setCause(causeReport.toString());
+        try {
+            uploadCrash.execute(new AsyncUtils.onExecuteListener<String>() {
+                @Override
+                public void preExecute() {
+                    String dateTime = new SimpleDateFormat("dd-MM-yyyy_hh.mm.ss", Locale.getDefault()).format(new Date());
+                    String fileName = "/crash-reports/crash_" + dateTime + ".log";
+                    FileUtils.with(app).debugLog(report.toString(), fileName);
+                }
+
+                @Override
+                public String doInBackground() {
+                    if (reportCrash) {
+                        String crashPath = FileUtils.with(app).getPersonalSpace(app) + "/crash-reports/";
+                        if (FileUtils.with(app).getNewestFile(crashPath) != null) {
+                            if (NetworkUtils.with(app).isConnectedTo(app)) {
+                                File crashFile = FileUtils.with(app).getNewestFile(crashPath);
+                                HttpUtils httpUtils = HttpUtils.get();
+                                HashMap<String, String> params = new HashMap<>();
+                                params.put("appName", crashModel.getAppName());
+                                params.put("versionCode", String.valueOf(crashModel.getVersionCode()));
+                                params.put("packageName", crashModel.getPackageName());
+                                params.put("versionName", crashModel.getVersionName());
+                                params.put("error", crashModel.getError());
+                                params.put("stackTrace", crashModel.getStackTrace());
+                                params.put("cause", crashModel.getCause());
+                                httpUtils.post(url, params);
+                                latch.countDown();
+                            }
+                        }
+                    }
+                    return null;
+                }
+
+                @Override
+                public void postDelayed() {
+                }
+            }, null);
+        } catch (InterruptedException ex) {
+            throw new RuntimeException(ex);
+        }
+        try {
+            if (!latch.await(2, TimeUnit.SECONDS)) {
+                Log.e("UncaughtException", "Timeout waiting for AsyncTask to complete");
+            }
+        } catch (InterruptedException ex) {
+            Log.e("UncaughtException", "InterruptedException while waiting for AsyncTask", ex);
+        }
+        defaultUEH.uncaughtException(t, e);
+    }
+
+    @NonNull
+    private StringBuilder getBuilder(StackTraceElement[] arr) {
+        StringBuilder stackTrace = new StringBuilder();
+        stackTrace.append("-------------------------------- Stack trace --------------------------------").append("\n");
+        report.append("-------------------------------- Stack trace --------------------------------").append("\n");
+        for (StackTraceElement stackTraceElement : arr) {
+            report.append(stackTraceElement.toString()).append("\n");
+            stackTrace.append(stackTraceElement).append("\n");
+        }
+        stackTrace.append("-----------------------------------------------------------------------------").append("\n").append("\n");
+        return stackTrace;
+    }
+
+    @NonNull
+    private StringBuilder getStringBuilder(StackTraceElement[] arr) {
+        StringBuilder causeReport = new StringBuilder();
+        report.append("----------------------------------- Cause -----------------------------------").append("\n");
+        causeReport.append("----------------------------------- Cause -----------------------------------").append("\n");
+        for (StackTraceElement stackTraceElement : arr) {
+            report.append(stackTraceElement.toString()).append("\n");
+            causeReport.append(stackTraceElement).append("\n");
+        }
+        report.append("-----------------------------------------------------------------------------").append("\n").append("\n");
+        causeReport.append("-----------------------------------------------------------------------------").append("\n").append("\n");
+        return causeReport;
+    }
+
+    /*private final Thread.UncaughtExceptionHandler defaultUEH;
     private final Activity app;
     private final String bearer;
     private final boolean reportCrash;
@@ -124,5 +251,5 @@ public class UncaughtExceptionHandler implements Thread.UncaughtExceptionHandler
             Log.e("UncaughtException", "InterruptedException while waiting for AsyncTask", ex);
         }
         defaultUEH.uncaughtException(t, e);
-    }
+    }*/
 }
